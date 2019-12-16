ce9617f7c4f29c7f2f2fffa764e63a50bfd590fb
vponnam <vponnam>
2019-12-16 10:48:47 -0500

    add commands for precise output

--- nfs-cleanup/droplet-read.sh
+++ nfs-cleanup/droplet-read.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-for yov in $(find /var/vcap/store/shared/cc-droplets/ -type f | grep -v buildpack_cache |cut -c 1-77)
- do
-   cd $yov
-   ls -ltch | awk 'NR>3 {print $9}' > /tmp/idrms
-   for dude in $(cat /tmp/idrms)
-    do
-    echo "older version droplet found with id: $dude"
-   done
- done
+> /tmp/idrms
+
+# check for more than 2 droplet versions
+for folder in $(find /var/vcap/store/shared/cc-droplets/ -type f | grep -v buildpack_cache |cut -c 1-77 | sort -u)
+do
+   ls -ltch $folder | awk 'NR>3 {print $9}' >> /tmp/idrms
+done
+
+# list the droplets
+for droplet in $(cat /tmp/idrms)
+do
+  echo "older version droplet found with id: $droplet"
+done
